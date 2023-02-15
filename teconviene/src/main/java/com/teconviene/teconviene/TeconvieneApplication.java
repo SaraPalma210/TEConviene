be641ebd980b447b846f8bd76ba330cb3eafb8de
david9V <davidvilches818@gmail.com>
2023-02-15 10:54:09 +0100

    02/23

---
+++ teconviene/src/main/java/com/teconviene/teconviene/TeconvieneApplication.java
@@ -0,0 +1,13 @@
+package com.teconviene.teconviene;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeconvieneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeconvieneApplication.class, args);
+	}
+
+}
---
+++ teconviene/src/test/java/com/teconviene/teconviene/TeconvieneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teconviene.teconviene;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeconvieneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
